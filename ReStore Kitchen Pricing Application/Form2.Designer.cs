b8639a324f1f8650947bdcbf66406579f61bc2e5
djinn1540 <gregoryjamesflynn@gmail.com>
2018-09-16 15:16:54 -0400

    actually moved the readme to the repo

---
+++ ReStore Kitchen Pricing Application/Form2.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ReStore_Kitchen_Pricing_Application
+{
+    partial class initialsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.initialsTextBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.promptLabel.Location = new System.Drawing.Point(38, 9);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(228, 25);
+            this.promptLabel.TabIndex = 0;
+            this.promptLabel.Text = "Please enter your initials:";
+            // 
+            // initialsTextBox
+            // 
+            this.initialsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.initialsTextBox.Location = new System.Drawing.Point(104, 58);
+            this.initialsTextBox.Name = "initialsTextBox";
+            this.initialsTextBox.Size = new System.Drawing.Size(100, 30);
+            this.initialsTextBox.TabIndex = 1;
+            // 
+            // okButton
+            // 
+            this.okButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.okButton.Location = new System.Drawing.Point(115, 94);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 30);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // initialsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 139);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.initialsTextBox);
+            this.Controls.Add(this.promptLabel);
+            this.Name = "initialsForm";
+            this.Text = "Initial Entry";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.TextBox initialsTextBox;
+        private System.Windows.Forms.Button okButton;
+    }
+}
---
+++ ReStore Kitchen Pricing Application/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReStore_Kitchen_Pricing_Application
+{
+    public partial class initialsForm : Form
+    {
+        kitchenForm parent = null;
+     
+
+        public initialsForm(kitchenForm parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (validInitials())
+            {
+                parent.initials = initialsTextBox.Text;
+                Console.Out.Write(initialsTextBox.Text);
+
+                if (parent.initials != null)
+                {
+                    parent.Enabled = true;
+                    parent.Visible = true;
+                    this.Close();
+                }
+                else
+                {
+                    throw new Exception("setting kitchenForm.initials from initialForm did not work");
+                }
+            }
+        }
+
+        public bool validInitials()
+        {
+            return (initialsTextBox.Text.Length == 2 || initialsTextBox.Text.Length == 3);
+    
+        }
+
+        public void getInitials()
+        {
+            this.Show();
+            
+            //this method is just supposed to transfer focus to the initialsForm
+        }
+        
+    }
+}
